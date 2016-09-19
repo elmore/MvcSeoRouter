c76aa05f51dee1cbacb3c6b2c6ab64b943053531
Tom Elmore <tom.elmore@easyroommate.com>
2016-09-19 12:36:36 +0100

    initial commit

---
+++ DemoWebsite/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoWebsite.Models;
+
+namespace DemoWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index(string seo)
+        {
+            var model = new HomeModel();
+
+            model.Seo = seo;
+
+            return View(model);
+        }
+
+    }
+}
---
+++ MvcSeoRouter/SeoRouter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web;
+using System.Web.Routing;
+
+namespace MvcSeoRouter
+{
+    public class SeoRoute : RouteBase
+    {
+        public override RouteData GetRouteData(HttpContextBase httpContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override VirtualPathData GetVirtualPath(RequestContext requestContext, RouteValueDictionary values)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
